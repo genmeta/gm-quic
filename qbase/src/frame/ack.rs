1e58a6601de42a0bcea730ed7a5b0c5b368aef52
huster-zhangpeng <zhangpeng@genmeta.net>
2024-04-21 16:58:52 +0800

    feat: add ack frame parser and writer in qbase

--- qbase/src/frame/ack.rs
+++ qbase/src/frame/ack.rs
@@ -1,4 +1,3 @@
-// ACK_FRAME, the most complex one
 // ACK Frame {
 //   Type (i) = 0x02..0x03,
 //   Largest Acknowledged (i),
@@ -8,3 +7,264 @@
 //   ACK Range (..) ...,
 //   [ECN Counts (..)],
 // }
+
+use crate::varint::{err::Error, VarInt};
+use std::{ops::RangeInclusive, vec::IntoIter};
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct AckFrame {
+    pub largest: VarInt,
+    pub delay: VarInt,
+    pub first_range: VarInt,
+    pub ranges: Vec<(VarInt, VarInt)>,
+    pub ecn: Option<EcnCounts>,
+}
+
+pub const ACK_FRAME_TYPE: u8 = 0x02;
+
+impl AckFrame {
+    pub fn new(
+        largest: u64,
+        first_range: u32,
+        delay: u64,
+        ecn: Option<EcnCounts>,
+    ) -> Result<Self, Error> {
+        Ok(Self {
+            largest: VarInt::try_from(largest)?,
+            delay: VarInt::try_from(delay)?,
+            first_range: VarInt::from_u32(first_range),
+            ranges: Vec::new(),
+            ecn,
+        })
+    }
+
+    /// Safe: It is impossible
+    pub fn alternating_gap_and_range(&mut self, gap: u32, range: u32) {
+        self.ranges
+            .push((VarInt::from_u32(gap), VarInt::from_u32(range)));
+    }
+}
+
+impl AckFrame {
+    pub fn take_ecn(&mut self) -> Option<EcnCounts> {
+        self.ecn.take()
+    }
+
+    /// Note: Calling `into_iter` will consume the ownership of the `AckFrame`.
+    /// Before doing so, it is important to handle the ECN information in the `AckFrame`.
+    pub fn into_iter(self) -> IntoAckIter {
+        IntoAckIter {
+            largest: self.largest.into_inner(),
+            first_range: Some(self.first_range.into_inner()),
+            iter: self.ranges.into_iter(),
+        }
+    }
+}
+
+pub struct IntoAckIter {
+    largest: u64,
+    first_range: Option<u64>,
+    iter: IntoIter<(VarInt, VarInt)>,
+}
+
+impl Iterator for IntoAckIter {
+    type Item = RangeInclusive<u64>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(first_range) = self.first_range.take() {
+            let largest = self.largest;
+            let smallest = largest - first_range;
+            self.largest = smallest;
+            Some(smallest..=largest)
+        } else {
+            self.iter.next().map(|(gap, range)| {
+                let largest = self.largest - gap.into_inner() - 2;
+                let smallest = largest - range.into_inner();
+                self.largest = smallest;
+                smallest..=largest
+            })
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub struct EcnCounts {
+    pub ect0: VarInt,
+    pub ect1: VarInt,
+    pub ce: VarInt,
+}
+
+pub mod ext {
+    use super::{AckFrame, EcnCounts};
+    use crate::frame::ack::ACK_FRAME_TYPE;
+    use crate::varint::ext::be_varint;
+    use nom::combinator::map;
+    use nom::sequence::tuple;
+
+    pub fn ack_frame_with_flag(ecn_flag: u8) -> impl Fn(&[u8]) -> nom::IResult<&[u8], AckFrame> {
+        move |input: &[u8]| {
+            let (mut input, (largest, delay, count, first_range)) =
+                tuple((be_varint, be_varint, be_varint, be_varint))(input)?;
+            let mut ranges = Vec::new();
+            let mut count = count.into_inner() as usize;
+            while count > 0 {
+                let (i, (gap, ack)) = tuple((be_varint, be_varint))(input)?;
+                ranges.push((gap, ack));
+                count -= 1;
+                input = i;
+            }
+
+            let ecn = if ecn_flag & 0x01 == 0x01 {
+                let (i, ecn) = be_ecn_counts(input)?;
+                input = i;
+                Some(ecn)
+            } else {
+                None
+            };
+
+            Ok((
+                input,
+                AckFrame {
+                    largest,
+                    delay,
+                    first_range,
+                    ranges,
+                    ecn,
+                },
+            ))
+        }
+    }
+
+    pub(super) fn be_ecn_counts(input: &[u8]) -> nom::IResult<&[u8], EcnCounts> {
+        map(
+            tuple((be_varint, be_varint, be_varint)),
+            |(ect0, ect1, ce)| EcnCounts { ect0, ect1, ce },
+        )(input)
+    }
+
+    pub trait BufMutExt {
+        fn put_ecn_counts(&mut self, ecn: &EcnCounts);
+        fn put_ack_frame(&mut self, frame: &AckFrame);
+    }
+
+    impl<T: bytes::BufMut> BufMutExt for T {
+        fn put_ecn_counts(&mut self, ecn: &EcnCounts) {
+            use crate::varint::ext::BufMutExt;
+            self.put_varint(&ecn.ect0);
+            self.put_varint(&ecn.ect1);
+            self.put_varint(&ecn.ce);
+        }
+
+        fn put_ack_frame(&mut self, frame: &AckFrame) {
+            use crate::varint::{ext::BufMutExt, VarInt};
+
+            let mut frame_type = ACK_FRAME_TYPE;
+            if frame.ecn.is_some() {
+                frame_type |= 0x1;
+            }
+            self.put_u8(frame_type);
+            self.put_varint(&frame.largest);
+            self.put_varint(&frame.delay);
+
+            let ack_range_count = VarInt::try_from(frame.ranges.len()).unwrap();
+            self.put_varint(&ack_range_count);
+            self.put_varint(&frame.first_range);
+            for (gap, ack) in &frame.ranges {
+                self.put_varint(gap);
+                self.put_varint(ack);
+            }
+            if let Some(ecn) = &frame.ecn {
+                self.put_ecn_counts(ecn);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        ext::{ack_frame_with_flag, be_ecn_counts, BufMutExt},
+        AckFrame, EcnCounts, ACK_FRAME_TYPE,
+    };
+    use crate::varint::{ext::be_varint, VarInt};
+    use nom::combinator::flat_map;
+
+    #[test]
+    fn test_read_ecn_count() {
+        let input = vec![0x52, 0x34, 0x52, 0x34, 0x52, 0x34];
+        let (input, ecn) = be_ecn_counts(&input).unwrap();
+        assert_eq!(input, &[]);
+        assert_eq!(
+            ecn,
+            EcnCounts {
+                ect0: VarInt::from_u32(0x1234),
+                ect1: VarInt::from_u32(0x1234),
+                ce: VarInt::from_u32(0x1234),
+            }
+        );
+    }
+
+    #[test]
+    fn test_read_ack_frame() {
+        let input = vec![0x02, 0x52, 0x34, 0x52, 0x34, 0x01, 0x52, 0x34, 3, 20];
+        let (input, ack_frame) = flat_map(be_varint, |frame_type| {
+            if frame_type.into_inner() as u8 == ACK_FRAME_TYPE {
+                ack_frame_with_flag(frame_type.into_inner() as u8)
+            } else {
+                panic!("wrong frame type")
+            }
+        })(&input)
+        .unwrap();
+        assert_eq!(input, &[]);
+        assert_eq!(
+            ack_frame,
+            AckFrame {
+                largest: VarInt::from_u32(0x1234),
+                delay: VarInt::from_u32(0x1234),
+                first_range: VarInt::from_u32(0x1234),
+                ranges: vec![(VarInt::from_u32(3), VarInt::from_u32(20))],
+                ecn: None,
+            }
+        );
+    }
+
+    #[test]
+    fn test_write_ecn_count() {
+        let mut buf = Vec::new();
+        let ecn = EcnCounts {
+            ect0: VarInt::from_u32(0x1234),
+            ect1: VarInt::from_u32(0x1234),
+            ce: VarInt::from_u32(0x1234),
+        };
+        buf.put_ecn_counts(&ecn);
+        assert_eq!(buf, vec![0x52, 0x34, 0x52, 0x34, 0x52, 0x34]);
+    }
+
+    #[test]
+    fn test_write_ack_frame() {
+        let mut buf = Vec::new();
+        let mut frame = AckFrame::new(0x1234, 0x1234, 0x1234, None).unwrap();
+        frame.alternating_gap_and_range(3, 20);
+
+        buf.put_ack_frame(&frame);
+        assert_eq!(
+            buf,
+            vec![0x02, 0x52, 0x34, 0x52, 0x34, 0x01, 0x52, 0x34, 3, 20]
+        );
+    }
+
+    #[test]
+    fn test_ack_frame_into_iter() {
+        let mut frame = AckFrame::new(1000, 0, 0x1234, None).unwrap();
+        frame.alternating_gap_and_range(0, 2);
+        frame.alternating_gap_and_range(4, 30);
+        frame.alternating_gap_and_range(7, 40);
+
+        let mut iter = frame.into_iter();
+        assert_eq!(iter.next(), Some(1000..=1000));
+        assert_eq!(iter.next(), Some(996..=998));
+        assert_eq!(iter.next(), Some(960..=990));
+        assert_eq!(iter.next(), Some(911..=951));
+        assert_eq!(iter.next(), None);
+    }
+}
--- qbase/src/frame/mod.rs
+++ qbase/src/frame/mod.rs
@@ -16,6 +16,7 @@ pub mod streams_blocked;
 
 use bytes::Bytes;
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum FrameType {
     Padding,
     Ping,
@@ -24,41 +25,47 @@ pub enum FrameType {
     MaxStreamData,
     MaxStreams(u8),
     StreamDataBlocked,
-    //Ack,
+    Ack(u8),
     Stream(u8),
     ResetStream,
     StopSending,
     Crypto,
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub struct InvalidFrameType(u8);
 
 impl TryFrom<u8> for FrameType {
     type Error = InvalidFrameType;
 
     fn try_from(frame_type: u8) -> Result<Self, Self::Error> {
-        match frame_type {
-            padding::PADDING_FRAME_TYPE => Ok(FrameType::Padding),
-            ping::PING_FRAME_TYPE => Ok(FrameType::Ping),
-            //ACK_FRAME_TYPE as u64 => FrameType::Ack,
-            reset_stream::RESET_STREAM_FRAME_TYPE => Ok(FrameType::ResetStream),
-            crypto::CRYPTO_FRAME_TYPE => Ok(FrameType::Crypto),
-            data_blocked::DATA_BLOCKED_FRAME_TYPE => Ok(FrameType::DataBlocked),
-            max_data::MAX_DATA_FRAME_TYPE => Ok(FrameType::MaxData),
-            max_stream_data::MAX_STREAM_DATA_FRAME_TYPE => Ok(FrameType::MaxStreamData),
-            0x12 | 0x13 => Ok(FrameType::MaxStreams(frame_type & 0b1)),
-            stop_sending::STOP_SENDING_FRAME_TYPE => Ok(FrameType::StopSending),
-            stream_data_blocked::STREAM_DATA_BLOCKED_FRAME_TYPE => Ok(FrameType::StreamDataBlocked),
-            8..=15 => Ok(FrameType::Stream(frame_type & 0b111)),
-            _ => Err(InvalidFrameType(frame_type)),
-        }
+        Ok(match frame_type {
+            padding::PADDING_FRAME_TYPE => FrameType::Padding,
+            ping::PING_FRAME_TYPE => FrameType::Ping,
+            0x2 | 0x3 => FrameType::Ack(frame_type & 0b1),
+            reset_stream::RESET_STREAM_FRAME_TYPE => FrameType::ResetStream,
+            crypto::CRYPTO_FRAME_TYPE => FrameType::Crypto,
+            data_blocked::DATA_BLOCKED_FRAME_TYPE => FrameType::DataBlocked,
+            max_data::MAX_DATA_FRAME_TYPE => FrameType::MaxData,
+            max_stream_data::MAX_STREAM_DATA_FRAME_TYPE => FrameType::MaxStreamData,
+            0x12 | 0x13 => FrameType::MaxStreams(frame_type & 0b1),
+            stop_sending::STOP_SENDING_FRAME_TYPE => FrameType::StopSending,
+            stream_data_blocked::STREAM_DATA_BLOCKED_FRAME_TYPE => FrameType::StreamDataBlocked,
+            8..=15 => FrameType::Stream(frame_type & 0b111),
+            _ => return Err(InvalidFrameType(frame_type)),
+        })
     }
 }
 
+/// 读取的Frame，其中涉及数据体的，得用Bytes来保存，因为数据体可能很大，
+/// 放在Frame结构中会很占空间，且有copy的性能开销；如果使用`&[u8]`,
+/// 则有生命周期的限制，而数据体实际上要等应用读取后，才可以释放，而并非
+/// 在读取解析完一个包后才释放。
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum ReadFrame {
     Padding(padding::PaddingFrame),
     Ping(ping::PingFrame),
-    //Ack(AckFrame),
+    Ack(ack::AckFrame),
     Stream(stream::StreamFrame, Bytes),
     ResetStream(reset_stream::ResetStreamFrame),
     Crypto(crypto::CryptoFrame, Bytes),
@@ -72,8 +79,9 @@ pub enum ReadFrame {
 
 pub mod ext {
     use super::{
-        crypto::ext::be_crypto_frame, data_blocked::ext::be_data_blocked_frame,
-        max_data::ext::be_max_data_frame, max_stream_data::ext::be_max_stream_data_frame,
+        ack::ext::ack_frame_with_flag, crypto::ext::be_crypto_frame,
+        data_blocked::ext::be_data_blocked_frame, max_data::ext::be_max_data_frame,
+        max_stream_data::ext::be_max_stream_data_frame,
         max_streams::ext::max_streams_frame_with_dir, padding::ext::be_padding_frame,
         ping::ext::be_ping_frame, reset_stream::ext::be_reset_stream_frame,
         stop_sending::ext::be_stop_sending_frame, stream::ext::stream_frame_with_flag,
@@ -81,9 +89,11 @@ pub mod ext {
     };
 
     use bytes::Bytes;
-    use nom::combinator::{flat_map, map, map_res};
-    use nom::error::{Error, ErrorKind};
-    use nom::{Err, IResult};
+    use nom::{
+        combinator::{flat_map, map, map_res},
+        error::{Error, ErrorKind},
+        Err, IResult,
+    };
 
     fn be_frame_type(input: &[u8]) -> IResult<&[u8], FrameType> {
         use crate::varint::ext::be_varint;
@@ -93,6 +103,7 @@ pub mod ext {
         })(input)
     }
 
+    /// Some frames like `STREAM` and `CRYPTO` have a data body, which use `bytes::Bytes` to store.
     fn complete_frame(
         frame_type: FrameType,
         raw: Bytes,
@@ -100,7 +111,7 @@ pub mod ext {
         move |input: &[u8]| match frame_type {
             FrameType::Padding => map(be_padding_frame, ReadFrame::Padding)(input),
             FrameType::Ping => map(be_ping_frame, ReadFrame::Ping)(input),
-            //FrameType::Ack => map_res(be_ack_frame, Frame::Ack)(input),
+            FrameType::Ack(ecn) => map(ack_frame_with_flag(ecn), ReadFrame::Ack)(input),
             FrameType::ResetStream => map(be_reset_stream_frame, ReadFrame::ResetStream)(input),
             FrameType::DataBlocked => map(be_data_blocked_frame, ReadFrame::DataBlocked)(input),
             FrameType::MaxData => map(be_max_data_frame, ReadFrame::MaxData)(input),
