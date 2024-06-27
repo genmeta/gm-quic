7a03961d533fc2d8803d01c363c1608bfc81fe91
metah3m <h3mlxf@gmail.com>
2024-06-27 18:52:34 +0800

    test(qcongestion): bbr test

--- qcongestion/src/bbr/parameters.rs
+++ qcongestion/src/bbr/parameters.rs
@@ -116,3 +116,114 @@ impl Bbr {
         (MIN_PIPE_CWND_PKTS * MSS) as u64
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_init_pacing_rate() {
+        let mut bbr = Bbr::new();
+        bbr.init();
+        assert_eq!(
+            bbr.pacing_rate,
+            (bbr.pacing_gain * INITIAL_CWND as f64 / INITIAL_RTT.as_secs_f64()) as u64
+        );
+    }
+
+    #[test]
+    fn test_bbr_set_pacing_rate() {
+        let mut bbr = Bbr::new();
+        bbr.btlbw = 1000;
+        bbr.is_filled_pipe = true;
+        bbr.set_pacing_rate();
+        assert_eq!(bbr.pacing_rate, (bbr.btlbw as f64 * bbr.pacing_gain) as u64);
+    }
+
+    #[test]
+    fn test_bbr_set_send_quantum() {
+        let mut bbr = Bbr::new();
+        bbr.pacing_rate = SEND_QUANTUM_THRESHOLD_PACING_RATE + 1;
+
+        bbr.set_send_quantum();
+        assert_eq!(bbr.send_quantum, (2 * MSS) as u64);
+
+        bbr.pacing_rate = SEND_QUANTUM_THRESHOLD_PACING_RATE - 1;
+        bbr.set_send_quantum();
+        assert_eq!(bbr.send_quantum, MSS as u64);
+
+        bbr.pacing_rate = 120_000_000;
+        bbr.set_send_quantum();
+        assert_eq!(bbr.send_quantum, 64 * 1024);
+
+        bbr.pacing_rate = 10_000_000;
+        bbr.set_send_quantum();
+        assert_eq!(bbr.send_quantum, 10000);
+    }
+
+    #[test]
+    fn test_bbr_inflight() {
+        let mut bbr = Bbr::new();
+        bbr.btlbw = 10_000_000;
+        bbr.rtprop = Duration::from_millis(100);
+        let bdp = bbr.inflight(1.0);
+        assert_eq!(bdp, 1_000_000);
+
+        bbr.send_quantum = 64 * 1024;
+
+        let bdp = bbr.inflight(1.0);
+        assert_eq!(bdp, 1_000_000 + bbr.send_quantum * 3);
+    }
+
+    #[test]
+    fn test_bbr_modulate_cwnd_for_recovery() {
+        let mut bbr = Bbr::new();
+
+        bbr.cwnd = 10000;
+        bbr.packet_conservation = false;
+        bbr.newly_lost_bytes = 1000;
+
+        // when packet lost cwnd sub lost_bytes
+        bbr.modulate_cwnd_for_recovery(9000);
+        assert_eq!(bbr.cwnd, 9000);
+
+        bbr.packet_conservation = true;
+        bbr.newly_lost_bytes = 0;
+        bbr.cwnd = 10000;
+        // when packet conservation cwnd add newly_acked_bytes
+        bbr.modulate_cwnd_for_recovery(9000);
+        bbr.newly_acked_bytes = 1000;
+        assert_eq!(bbr.cwnd, 10000);
+    }
+
+    #[test]
+    fn test_modulate_cwnd_for_probe_rtt() {
+        let mut bbr = Bbr::new();
+        bbr.cwnd = 10000;
+        // min(4 * MSS, cwnd)
+        bbr.state = BbrStateMachine::ProbeRTT;
+        bbr.modulate_cwnd_for_probe_rtt();
+
+        assert_eq!(bbr.cwnd, (4 * MSS) as u64);
+    }
+
+    #[test]
+    fn test_bbr_set_cwnd() {
+        let mut bbr = Bbr::new();
+
+        bbr.bytes_in_flight = 1000;
+        bbr.packet_conservation = false;
+        bbr.btlbw = 10_000_000; // 10Mbps
+        bbr.rtprop = Duration::from_millis(100);
+        bbr.newly_acked_bytes = 4000;
+
+        // pacing_rate = btlbw * pacing_gain
+        // target_cwnd = (btlbw * rtt) * cwnd_gain + quantum
+
+        // init cwnd < tartget_cwnd
+        // when receive ack, adjust cwnd
+        bbr.set_cwnd();
+        assert_eq!(bbr.cwnd, 100000);
+    }
+}
