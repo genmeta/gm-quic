86328293212fcbc1e2432368627275f470d1d9ea
huster-zhangpeng <zhangpeng@genmeta.net>
2024-09-18 18:29:45 +0800

    docs(qbase): add docs for handshake

--- qbase/src/handshake.rs
+++ qbase/src/handshake.rs
@@ -9,29 +9,45 @@ use crate::{
     streamid::Role,
 };
 
+/// The completion flag for the client handshake.
+///
+/// The client considers the handshake complete only after
+/// receiving the [`HandshakeDoneFrame`] from the server.
+/// In the QUIC protocol, there are no tasks that specifically
+/// require waiting for the client handshake to complete.
+/// Instead, it simply queries the handshake status.
 #[derive(Debug, Default, Clone)]
 pub struct ClientHandshake(Arc<AtomicBool>);
 
 impl ClientHandshake {
-    fn is_handshake_done(&self) -> bool {
+    /// Check if the client handshake is complete.
+    pub fn is_handshake_done(&self) -> bool {
         self.0.load(Ordering::Acquire)
     }
 
-    fn recv_handshake_done_frame(&self, _frame: &HandshakeDoneFrame) {
+    /// Receive the HANDSHAKE_DONE frame.
+    ///
+    /// Once the client receives the HANDSHAKE_DONE frame,
+    /// it marks the completion of the client handshake.
+    pub fn recv_handshake_done_frame(&self, _frame: &HandshakeDoneFrame) {
         self.0.store(true, Ordering::Release);
     }
 }
 
-/// Server handshake status, divided into:
-/// - Ok(Some(false)), handshake not successful, initial state;
-/// - Err(waker), external query before handshake is successful, and external
-///   waiting for handshake to complete to send HandshakeDone frame;
-/// - Ok(Some(true)), handshake successful, changed to this state through the
-///   done function, and waker is awakened.
-/// - Ok(None) indicates that the handshake was abandoned halfway, the connection
-///   ended before the handshake was completed.
+/// Server's handshake status.
 ///
-/// The handshake is considered complete when the client's 1rtt data packet is successfully decrypted.
+/// - `T` is responsible for reliably sending [`HandshakeDoneFrame`] to the client.
+///    It can be a channel, a queue, or a buffer. Whatever, it must be able to send the
+///    [`HandshakeDoneFrame`] to the client.
+///
+/// The server considers the handshake complete only after receiving
+/// the [finished message](https://www.rfc-editor.org/rfc/rfc8446.html#section-4.4.4)
+/// from the client during the TLS handshake process.
+/// If the [finished message](https://www.rfc-editor.org/rfc/rfc8446.html#section-4.4.4)
+/// from the TLS handshake is not received,
+/// the server can also consider the handshake complete upon receiving and
+/// successfully decrypting the client's 1-RTT packet.
+/// Once the server's handshake is complete, the server will send a [`HandshakeDoneFrame`] immediately.
 #[derive(Debug, Clone)]
 pub struct ServerHandshake<T>
 where
@@ -45,18 +61,34 @@ impl<T> ServerHandshake<T>
 where
     T: SendFrame<HandshakeDoneFrame> + Clone,
 {
-    fn new(output: T) -> Self {
+    /// Create a new server handshake signal.
+    ///
+    /// The `output` is responsible for sending the [`HandshakeDoneFrame`] to the client,
+    /// see [`ServerHandshake`].
+    pub fn new(output: T) -> Self {
         ServerHandshake {
             is_done: Arc::new(AtomicBool::new(false)),
             output,
         }
     }
 
-    fn is_handshake_done(&self) -> bool {
+    /// Check if the server handshake is complete.
+    pub fn is_handshake_done(&self) -> bool {
         self.is_done.load(Ordering::Acquire)
     }
 
-    fn done(&self) {
+    /// Actively set the server's handshake status to complete.
+    ///
+    /// Call this method when the TLS handshake
+    /// [finished message](https://www.rfc-editor.org/rfc/rfc8446.html#section-4.4.4) is received.
+    /// If the TLS handshake completion message is not received,
+    /// receiving and successfully decrypting the client's 1-RTT packet
+    /// is also considered handshake completion.
+    /// Servers MUST NOT send a [`HandshakeDoneFrame`] before completing the handshake.
+    /// and once the server handshake is complete,
+    /// servers should send the [`HandshakeDoneFrame`] immediately.
+    /// See [`ServerHandshake`].
+    pub fn done(&self) {
         if self
             .is_done
             .compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire)
@@ -67,12 +99,18 @@ where
     }
 }
 
+/// A merged handshake state that can be used by both the client and the server.
+///
+/// For convenience, a unified [`Handshake`]` should be used,
+/// which will internally choose the corresponding behavior based on the role.
 #[derive(Debug, Clone)]
 pub enum Handshake<T>
 where
     T: SendFrame<HandshakeDoneFrame> + Clone,
 {
+    /// The client's handshake state if the endpoint is a client.
     Client(ClientHandshake),
+    /// The server's handshake state if the endpoint is a server.
     Server(ServerHandshake<T>),
 }
 
@@ -80,6 +118,7 @@ impl<T> Handshake<T>
 where
     T: SendFrame<HandshakeDoneFrame> + Clone,
 {
+    /// Create a new handshake state, based on the role.
     pub fn new(role: Role, output: T) -> Self {
         match role {
             Role::Client => Handshake::Client(ClientHandshake::default()),
@@ -87,14 +126,19 @@ where
         }
     }
 
+    /// Create a new client handshake state.
     pub fn new_client() -> Self {
         Handshake::Client(ClientHandshake::default())
     }
 
+    /// Create a new server handshake state.
+    /// The `output` is responsible for sending the [`HandshakeDoneFrame`] to the client,
+    /// see [`ServerHandshake::new`].
     pub fn new_server(output: T) -> Self {
         Handshake::Server(ServerHandshake::new(output))
     }
 
+    /// Check if the handshake is complete.
     pub fn is_handshake_done(&self) -> bool {
         match self {
             Handshake::Client(h) => h.is_handshake_done(),
@@ -102,7 +146,11 @@ where
         }
     }
 
-    /// Just like `recv_handshake_done_frame`, a client must wait for a HANDSHAKE_DONE frame to be done.
+    /// Actively set the handshake status to complete.
+    ///
+    /// For the server, it actively sets the handshake status to complete, see [`ServerHandshake::done`].
+    /// However, for the client, there is no active setting operation;
+    /// a client must wait for a HANDSHAKE_DONE frame to be done.
     /// So as a client, it just print a warning log.
     pub fn done(&self) {
         match self {
@@ -111,6 +159,7 @@ where
         }
     }
 
+    /// Return the role of this handshake signal.
     pub fn role(&self) -> Role {
         match self {
             Handshake::Client(_) => Role::Client,
@@ -119,16 +168,19 @@ where
     }
 }
 
-/// See [RFC 9000 section 19.20](https://www.rfc-editor.org/rfc/rfc9000.html#section-19.20):
-/// A HANDSHAKE_DONE frame can only be sent by the server. Servers MUST NOT send a HANDSHAKE_DONE
-/// frame before completing the handshake. A server MUST treat receipt of a HANDSHAKE_DONE frame
-/// as a connection error of type PROTOCOL_VIOLATION.
 impl<T> ReceiveFrame<HandshakeDoneFrame> for Handshake<T>
 where
     T: SendFrame<HandshakeDoneFrame> + Clone,
 {
     type Output = ();
 
+    /// Receive the [`HandshakeDoneFrame`].
+    ///
+    /// A [`HandshakeDoneFrame`] can only be received by the client.
+    /// A server MUST treat receipt of a [`HandshakeDoneFrame`]
+    /// as a connection error of type PROTOCOL_VIOLATION.
+    /// See [section 19.20](https://www.rfc-editor.org/rfc/rfc9000.html#section-19.20)
+    /// of [QUIC](https://www.rfc-editor.org/rfc/rfc9000.html).
     fn recv_frame(&self, frame: &HandshakeDoneFrame) -> Result<(), Error> {
         match self {
             Handshake::Client(h) => {
