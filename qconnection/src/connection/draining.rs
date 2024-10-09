91c67c9656743d99408bac38d562ea6704937517
yiyue-reimu <reimu@genmeta.net>
2024-10-09 10:21:24 +0800

    fix(qconnection): make ArcConenction::close sync, and other bugs fix

--- qconnection/src/connection/draining.rs
+++ qconnection/src/connection/draining.rs
@@ -1,6 +1,6 @@
 use qbase::packet::{header::GetType, DataPacket};
 
-use super::{closing::ClosingConnection, raw::RawConnection, ArcLocalCids};
+use super::ArcLocalCids;
 
 /// Connection in draining state, entered from the raw state or closing state.
 /// It just ignores all packets, and waits for dismissing.
@@ -8,15 +8,10 @@ use super::{closing::ClosingConnection, raw::RawConnection, ArcLocalCids};
 #[derive(Debug)]
 pub struct DrainingConnection(ArcLocalCids);
 
-impl From<RawConnection> for DrainingConnection {
-    fn from(value: RawConnection) -> Self {
-        Self(value.cid_registry.local)
-    }
-}
-
-impl From<ClosingConnection> for DrainingConnection {
-    fn from(value: ClosingConnection) -> Self {
-        Self(value.cid_registry.local)
+impl DrainingConnection {
+    /// Create a new draining connection
+    pub fn new(local_cids: ArcLocalCids) -> Self {
+        Self(local_cids)
     }
 }
 
