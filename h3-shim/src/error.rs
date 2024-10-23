1c1ed82c6b84c77177e8c87f7dbf0108bc3c07e0
huster-zhangpeng <zhangpeng@genmeta.net>
2024-10-23 17:41:25 +0800

    refactor(h3-shim): split error into independent module

---
+++ h3-shim/src/error.rs
@@ -0,0 +1,54 @@
+use std::{error, fmt, io, sync::Arc};
+
+use qbase::frame::ResetStreamError;
+
+#[derive(Clone)]
+pub struct Error(Arc<io::Error>);
+
+impl fmt::Display for Error {
+    #[inline]
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl fmt::Debug for Error {
+    #[inline]
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl error::Error for Error {
+    #[inline]
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        self.0.source()
+    }
+}
+
+impl h3::quic::Error for Error {
+    #[inline]
+    fn is_timeout(&self) -> bool {
+        false
+    }
+
+    #[inline]
+    fn err_code(&self) -> Option<u64> {
+        error::Error::source(self)
+            .and_then(|e| e.downcast_ref::<ResetStreamError>().map(|e| e.error_code()))
+    }
+}
+
+impl From<io::Error> for Error {
+    #[inline]
+    fn from(value: io::Error) -> Self {
+        Self(value.into())
+    }
+}
+
+impl From<ResetStreamError> for Error {
+    #[inline]
+    fn from(value: ResetStreamError) -> Self {
+        io::Error::new(io::ErrorKind::BrokenPipe, value).into()
+    }
+}
