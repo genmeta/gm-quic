c77446c72fa7173ea842ad5d1529fc0dd285d274
eareimu <reimu@genmeta.net>
2024-11-14 17:00:46 +0800

    feat(qbase): differentiate PacketWriter and impl WriteFrames

--- qbase/src/packet.rs
+++ qbase/src/packet.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use bytes::{buf::UninitSlice, BufMut, BytesMut};
 use deref_derive::{Deref, DerefMut};
 use encrypt::{encode_long_first_byte, encode_short_first_byte, encrypt_packet, protect_header};
@@ -6,11 +8,7 @@ use header::io::WriteHeader;
 
 use crate::{
     cid::ConnectionId,
-    frame::{
-        io::{WriteDataFrame, WriteFrame},
-        BeFrame, ContainSpec, Spec,
-    },
-    util::DescribeData,
+    frame::io::{WriteDataFrame, WriteFrame},
     varint::{EncodeBytes, VarInt, WriteVarInt},
 };
 
@@ -144,13 +142,12 @@ impl Iterator for PacketReader {
     }
 }
 
-pub struct PacketWriter<'b> {
+pub struct PacketWriter<'b, H> {
     buffer: &'b mut [u8],
     hdr_len: usize,
     len_encoding: usize,
     pn: (u64, PacketNumber),
     cursor: usize,
-    end: usize,
     tag_len: usize,
 
     // Packets containing only frames with [`Spec::N`] are not ack-eliciting;
@@ -166,11 +163,13 @@ pub struct PacketWriter<'b> {
     in_flight: bool,
     // Packets containing only frames with [`Spec::P`] can be used to
     // probe new network paths during connection migration.
-    _probe_new_path: bool,
+    probe_new_path: bool,
+
+    _kind: PhantomData<H>,
 }
 
-impl<'b> PacketWriter<'b> {
-    pub fn new<H>(
+impl<'b, H> PacketWriter<'b, H> {
+    pub fn new(
         header: &H,
         buffer: &'b mut [u8],
         pn: (u64, PacketNumber),
@@ -191,46 +190,23 @@ impl<'b> PacketWriter<'b> {
         hdr_buf.put_header(header);
         payload_buf.put_packet_number(encoded_pn);
 
-        let end = buffer.len() - tag_len;
+        let cursor = hdr_len + len_encoding + encoded_pn.size();
         Some(Self {
             buffer,
             hdr_len,
             len_encoding,
             pn,
-            cursor: hdr_len + len_encoding + encoded_pn.size(),
-            end,
+            cursor,
             tag_len,
             ack_eliciting: false,
             in_flight: false,
-            _probe_new_path: false,
+            probe_new_path: false,
+            _kind: PhantomData,
         })
     }
 
     pub fn pad(&mut self, cnt: usize) {
         self.put_bytes(0, cnt);
-    }
-
-    pub fn write_frame<F>(&mut self, frame: &F)
-    where
-        F: BeFrame,
-        Self: WriteFrame<F>,
-    {
-        let specs = frame.frame_type().specs();
-        self.ack_eliciting |= !specs.contain(Spec::N);
-        self.in_flight |= !specs.contain(Spec::C);
-
-        self.put_frame(frame);
-    }
-
-    pub fn write_data_frame<F, D>(&mut self, frame: &F, data: &D)
-    where
-        F: BeFrame,
-        D: DescribeData,
-        Self: WriteDataFrame<F, D>,
-    {
-        self.ack_eliciting = true;
-        self.in_flight = true;
-        self.put_data_frame(frame, data);
     }
 
     #[inline]
@@ -239,67 +215,126 @@ impl<'b> PacketWriter<'b> {
     }
 
     #[inline]
-    pub fn in_flight(&self) -> bool {
+    pub fn is_in_flight(&self) -> bool {
         self.in_flight
-    }
-
-    pub fn encrypt_long_packet(
-        &mut self,
-        hpk: &dyn rustls::quic::HeaderProtectionKey,
-        pk: &dyn rustls::quic::PacketKey,
-    ) -> usize {
-        let (actual_pn, encoded_pn) = self.pn;
-        encode_long_first_byte(&mut self.buffer[0], encoded_pn.size());
-
-        let pkt_size = self.cursor + self.tag_len;
-        let payload_len = pkt_size - self.hdr_len - self.len_encoding;
-        let mut pn_buf = &mut self.buffer[self.hdr_len..self.hdr_len + self.len_encoding];
-        pn_buf.encode_varint(&VarInt::try_from(payload_len).unwrap(), EncodeBytes::Two);
-
-        encrypt_packet(
-            pk,
-            actual_pn,
-            &mut self.buffer[..pkt_size],
-            self.hdr_len + self.len_encoding + encoded_pn.size(),
-        );
-        protect_header(
-            hpk,
-            &mut self.buffer[..pkt_size],
-            self.hdr_len,
-            encoded_pn.size(),
-        );
-        pkt_size
-    }
-
-    pub fn encrypt_short_packet(
-        &mut self,
-        key_phase: KeyPhaseBit,
-        hpk: &dyn rustls::quic::HeaderProtectionKey,
-        pk: &dyn rustls::quic::PacketKey,
-    ) -> usize {
-        let (actual_pn, encoded_pn) = self.pn;
-        encode_short_first_byte(&mut self.buffer[0], encoded_pn.size(), key_phase);
-
-        let pkt_size = self.cursor + self.tag_len;
-        encrypt_packet(
-            pk,
-            actual_pn,
-            &mut self.buffer[..pkt_size],
-            self.hdr_len + self.len_encoding + encoded_pn.size(),
-        );
-        protect_header(
-            hpk,
-            &mut self.buffer[..pkt_size],
-            self.hdr_len,
-            encoded_pn.size(),
-        );
-        pkt_size
     }
 }
 
-unsafe impl BufMut for PacketWriter<'_> {
+macro_rules! impl_packet_writer {
+    (@long $lh_ty:ty) => {
+        impl PacketWriter<'_, $lh_ty> {
+            /// Seal the packet and return the remaining length.
+            ///
+            /// Once the packet is encapsulated, you cannot write any more data to it, you can still encrypt it.
+            pub fn seal_packet(&mut self) -> &mut [u8] {
+                let buffer = core::mem::take(&mut self.buffer);
+
+                let (packet, remain) = buffer.split_at_mut(self.cursor + self.tag_len);
+                self.buffer = packet;
+
+                remain
+            }
+
+            /// Extend the packet with the next contiguous buffer.
+            ///
+            /// This is useful when you regret sealing a packet.
+            ///
+            /// Panic if the given buffer is not contiguous with the old buffer.
+            ///
+            /// ``` example, no_run
+            /// # use qbase::packet::PacketWriter;
+            /// # use qbase::packet::header::InitialHeader;
+            /// # let packet_writer: PacketWriter<InitialHeader> = unimplemented!();
+            /// let remain = packet_writer.seal_packet();
+            /// packet_buffer.extend_packet(remain);
+            /// ```
+            pub fn extend_packet(&mut self, next: &mut [u8]) {
+                if self.buffer.as_ptr().wrapping_add(self.buffer.len()) == next.as_ptr() {
+                    let data = self.buffer.as_mut_ptr();
+                    let len = self.buffer.len() + next.len();
+                    // SAFETY: the new buffer is contiguous with the old buffer, they can be merged.
+                    self.buffer = unsafe { core::slice::from_raw_parts_mut(data, len) };
+                } else {
+                    panic!("extend_packet: the new buffer is not contiguous with the old buffer");
+                }
+            }
+
+            pub fn encrypt(
+                &mut self,
+                hpk: &dyn rustls::quic::HeaderProtectionKey,
+                pk: &dyn rustls::quic::PacketKey,
+            ) -> usize {
+                let (actual_pn, encoded_pn) = self.pn;
+                encode_long_first_byte(&mut self.buffer[0], encoded_pn.size());
+
+                let pkt_size = self.buffer.len();
+                let payload_len = pkt_size - self.hdr_len - self.len_encoding;
+                let mut pn_buf = &mut self.buffer[self.hdr_len..self.hdr_len + self.len_encoding];
+                pn_buf.encode_varint(&VarInt::try_from(payload_len).unwrap(), EncodeBytes::Two);
+
+                encrypt_packet(
+                    pk,
+                    actual_pn,
+                    &mut self.buffer[..pkt_size],
+                    self.hdr_len + self.len_encoding + encoded_pn.size(),
+                );
+                protect_header(
+                    hpk,
+                    &mut self.buffer[..pkt_size],
+                    self.hdr_len,
+                    encoded_pn.size(),
+                );
+                pkt_size
+            }
+        }
+    };
+    (@short $sh_ty:ty) => {
+        impl PacketWriter<'_, $sh_ty> {
+            pub fn encrypt(
+                &mut self,
+                key_phase: KeyPhaseBit,
+                hpk: &dyn rustls::quic::HeaderProtectionKey,
+                pk: &dyn rustls::quic::PacketKey,
+            ) -> usize {
+                let (actual_pn, encoded_pn) = self.pn;
+                encode_short_first_byte(&mut self.buffer[0], encoded_pn.size(), key_phase);
+
+                let pkt_size = self.buffer.len();
+                encrypt_packet(
+                    pk,
+                    actual_pn,
+                    &mut self.buffer[..pkt_size],
+                    self.hdr_len + self.len_encoding + encoded_pn.size(),
+                );
+                protect_header(
+                    hpk,
+                    &mut self.buffer[..pkt_size],
+                    self.hdr_len,
+                    encoded_pn.size(),
+                );
+                pkt_size
+            }
+        }
+    };
+    (@long $($lh_ty:ty)* $(;$($remain:tt)*)?) => {
+        $(impl_packet_writer!(@long $lh_ty);)*
+        $(impl_packet_writer!($($remain)*); )?
+
+    };
+    (@short $($sh_ty:ty)* $(;$($remain:tt)*)?) => {
+        $(impl_packet_writer!(@short $sh_ty);)*
+        $(impl_packet_writer!($($remain)*); )?
+    };
+}
+
+impl_packet_writer! {
+    @long InitialHeader HandshakeHeader RetryHeader ZeroRttHeader;
+    @short OneRttHeader
+}
+
+unsafe impl<H> BufMut for PacketWriter<'_, H> {
     fn remaining_mut(&self) -> usize {
-        self.end - self.cursor
+        self.buffer.len() - self.tag_len - self.cursor
     }
 
     unsafe fn advance_mut(&mut self, cnt: usize) {
@@ -315,6 +350,114 @@ unsafe impl BufMut for PacketWriter<'_> {
     }
 
     fn chunk_mut(&mut self) -> &mut UninitSlice {
-        UninitSlice::new(&mut self.buffer[self.cursor..self.end])
+        let remaining_len = self.remaining_mut();
+        UninitSlice::new(&mut self.buffer[self.cursor..][..remaining_len])
     }
+}
+
+macro_rules! impl_packet_writer_write_frames {
+    // 和FrameType::specs重复了
+    (@spec $this:ident NP) => {                                                    $this.probe_new_path = true; };
+    (@spec $this:ident NC) => {                                                                                 };
+    (@spec $this:ident P ) => { $this.ack_eliciting = true; $this.in_flight = true; $this.probe_new_path = true; };
+    (@spec $this:ident N ) => {                             $this.in_flight = true;                              };
+    (@spec $this:ident   ) => { $this.ack_eliciting = true; $this.in_flight = true;                              };
+    // 实现 WriteFrame
+    (@imp_wf  $frame:ident : $hdr:ident $($spec:ident)?) => {
+        impl WriteFrame<$crate::frame::$frame> for PacketWriter<'_, $hdr> {
+            fn put_frame(&mut self, frame: &$crate::frame::$frame) {
+                impl_packet_writer_write_frames!(@spec self $($spec)?);
+                // same as BytesMut::chunk_mut
+                let body_remaining_len = self.remaining_mut();
+                let mut body = &mut self.buffer[self.cursor..][..body_remaining_len];
+
+                body.put_frame(frame);
+            }
+        }
+    };
+    // 实现 WriteDataFrame
+    (@imp_wdf $frame:ident : $hdr:ident $($spec:ident)?) => {
+        impl<D> WriteDataFrame<$crate::frame::$frame, D> for PacketWriter<'_, $hdr>
+        where
+            D: crate::util::DescribeData,
+            for<'a> &'a mut [u8]: WriteDataFrame<$crate::frame::$frame, D>
+        {
+            fn put_data_frame(&mut self, frame: &$crate::frame::$frame, data: &D) {
+                impl_packet_writer_write_frames!(@spec self $($spec)?);
+                // same as BytesMut::chunk_mut
+                let body_remaining_len = self.remaining_mut();
+                let mut body = &mut self.buffer[self.cursor..][..body_remaining_len];
+
+                body.put_data_frame(frame, data);
+            }
+        }
+    };
+    // 根据$frame选择需要实现的trait：筛选出DataFrame和非DataFrame
+    (@imp  StreamFrame : $hdr:ident $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp_wdf StreamFrame  : $hdr $($spec)?);
+    };
+    (@imp  CryptoFrame : $hdr:ident $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp_wdf CryptoFrame  : $hdr $($spec)?);
+    };
+    (@imp  DatagramFrame : $hdr:ident $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp_wdf DatagramFrame: $hdr $($spec)?);
+    };
+    (@imp  $frame:ident : $hdr:ident $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp_wf  $frame       : $hdr $($spec)?);
+    };
+    // 实现对各种Header的实现
+    (@pkts $frame:ident: IH01 $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp $frame: InitialHeader   $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: HandshakeHeader $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: ZeroRttHeader   $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: OneRttHeader    $($spec)?);
+    };
+    (@pkts $frame:ident: IH_1 $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp $frame: InitialHeader   $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: HandshakeHeader $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: OneRttHeader    $($spec)?);
+    };
+    (@pkts $frame:ident: __01 $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp $frame: HandshakeHeader $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: OneRttHeader    $($spec)?);
+    };
+    (@pkts $frame:ident: ___1 $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp $frame: OneRttHeader    $($spec)?);
+    };
+    (@pkts $frame:ident: ih01 $($spec:ident)?) => {
+        impl_packet_writer_write_frames!(@imp $frame: InitialHeader   $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: HandshakeHeader $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: ZeroRttHeader   $($spec)?);
+        impl_packet_writer_write_frames!(@imp $frame: OneRttHeader    $($spec)?);
+    };
+    ($( $frame:ident: $pkts:ident $($spec:ident)? ;)*) => {
+        $(impl_packet_writer_write_frames!(@pkts $frame: $pkts $($spec)?);)*
+    }
+}
+
+// 与 https://www.rfc-editor.org/rfc/rfc9000.html#frame-types 对应
+impl_packet_writer_write_frames! {
+//  Frame Type Name           Pkts Spec
+    PaddingFrame            : IH01 NP  ;
+    PingFrame               : IH01     ;
+    AckFrame                : IH01 NC  ;
+    ResetStreamFrame        : __01     ;
+    StopSendingFrame        : __01     ;
+    CryptoFrame             : IH_1     ;
+    NewTokenFrame           : ___1 NC  ;
+    StreamFrame             : ih01     ;
+    MaxDataFrame            : __01     ;
+    MaxStreamDataFrame      : __01     ;
+    MaxStreamsFrame         : __01     ;
+    DataBlockedFrame        : __01     ;
+    StreamDataBlockedFrame  : __01     ;
+    StreamsBlockedFrame     : __01     ;
+    NewConnectionIdFrame    : __01 P   ;
+    RetireConnectionIdFrame : __01     ;
+    PathChallengeFrame      : __01 P   ;
+    PathResponseFrame       : __01 P   ;
+    ConnectionCloseFrame    : ih01 N   ;
+    HandshakeDoneFrame      : ___1     ;
+    // rfc9221
+    DatagramFrame           : __01 N   ;
 }
