7bc7d9dbde0bf63245489dfbbcd19459302b0425
huster-zhangpeng <zhangpeng@genmeta.net>
2025-01-01 20:44:44 +0800

    feat: let flow ctrl return quic Error, add router for new qconnection

---
+++ qconnection3/src/router/signpost.rs
@@ -0,0 +1,27 @@
+use std::net::SocketAddr;
+
+use qbase::cid::{self, ConnectionId};
+
+#[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
+pub struct Signpost {
+    cid: ConnectionId,
+    peer: Option<SocketAddr>,
+}
+
+impl From<ConnectionId> for Signpost {
+    fn from(value: ConnectionId) -> Self {
+        Self {
+            cid: value,
+            peer: None,
+        }
+    }
+}
+
+impl From<SocketAddr> for Signpost {
+    fn from(value: SocketAddr) -> Self {
+        Self {
+            cid: cid::ConnectionId::default(),
+            peer: Some(value),
+        }
+    }
+}
