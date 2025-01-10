bdd82d06df267f90f8c1236ff8bb9b90b32f9076
huster-zhangpeng <zhangpeng@genmeta.net>
2025-01-10 11:39:13 +0800

    refactor: remove path::paths module

--- qconnection4/src/path.rs
+++ qconnection4/src/path.rs
@@ -2,22 +2,23 @@ use std::{
     io::{self},
     net::SocketAddr,
     sync::{Arc, Mutex},
+    time::Duration,
 };
 
-mod aa;
-pub mod burst;
-mod paths;
-pub use aa::*;
-pub use paths::*;
-
-mod util;
+use dashmap::DashMap;
 use qbase::{
     error::Error,
     frame::{PathChallengeFrame, PathResponseFrame, ReceiveFrame},
+    Epoch,
 };
-use qcongestion::ArcCC;
+use qcongestion::{ArcCC, CongestionControl};
 use qinterface::{conn::ConnInterface, path::Pathway, SendCapability};
-use tokio::time::Instant;
+use tokio::{task::AbortHandle, time::Instant};
+
+mod aa;
+pub use aa::*;
+pub mod burst;
+mod util;
 pub use util::*;
 
 pub struct Path {
@@ -49,7 +50,6 @@ impl Path {
     pub async fn validate(&self) -> bool {
         let challenge = PathChallengeFrame::random();
         for _ in 0..3 {
-            use qcongestion::CongestionControl;
             let pto = self.cc.pto_time(qbase::Epoch::Data);
             self.challenge_sndbuf.write(challenge);
             match tokio::time::timeout(pto, self.response_rcvbuf.receive()).await {
@@ -109,5 +109,81 @@ impl ReceiveFrame<PathResponseFrame> for Path {
     fn recv_frame(&self, frame: &PathResponseFrame) -> Result<Self::Output, Error> {
         self.response_rcvbuf.write(*frame);
         Ok(())
+    }
+}
+
+pub struct PathGuard {
+    path: Arc<Path>,
+    task: AbortHandle,
+}
+
+impl PathGuard {
+    pub fn new(path: Arc<Path>, task: AbortHandle) -> Self {
+        Self { path, task }
+    }
+}
+
+impl Drop for PathGuard {
+    fn drop(&mut self) {
+        self.task.abort();
+    }
+}
+
+#[derive(Clone)]
+pub struct ArcPaths {
+    paths: Arc<DashMap<Pathway, PathGuard>>,
+    ticker: AbortHandle,
+}
+
+impl Default for ArcPaths {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ArcPaths {
+    pub fn new() -> Self {
+        let paths: Arc<DashMap<Pathway, PathGuard>> = Arc::new(DashMap::new());
+        let ticker = tokio::spawn({
+            let arc_paths = paths.clone();
+            async move {
+                loop {
+                    tokio::time::sleep(Duration::from_micros(10)).await;
+                    for path_guard in arc_paths.iter() {
+                        path_guard.path.cc.do_tick();
+                    }
+                }
+            }
+        })
+        .abort_handle();
+        Self { paths, ticker }
+    }
+
+    pub fn get(&self, pathway: &Pathway) -> Option<Arc<Path>> {
+        self.paths.get(pathway).map(|guard| guard.path.clone())
+    }
+
+    pub fn entry(&self, pathway: Pathway) -> dashmap::Entry<'_, Pathway, PathGuard> {
+        self.paths.entry(pathway)
+    }
+
+    pub fn del(&self, pathway: &Pathway) {
+        self.paths.remove(pathway);
+    }
+
+    pub fn exist_paths(&self) -> usize {
+        self.paths.len()
+    }
+
+    pub fn max_pto_duration(&self) -> Option<Duration> {
+        self.paths
+            .iter()
+            .map(|guard| guard.path.cc.pto_time(Epoch::Data))
+            .max()
+    }
+
+    pub fn clear(&self) {
+        self.ticker.abort();
+        self.paths.clear();
     }
 }
