3adc65c8c3e67e1b791d3e58d114c99cf5e28433
eareimu <reimu@genmeta.net>
2025-02-17 16:26:24 +0800

    feat(qlog): basic telemetry collector

--- qlog/src/loglevel.rs
+++ qlog/src/loglevel.rs
@@ -1,30 +1,52 @@
+use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 
 #[serde_with::skip_serializing_none]
-#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Builder, Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[builder(
+    default,
+    setter(into, strip_option),
+    build_fn(private, name = "fallible_build")
+)]
 pub struct Error {
     pub code: Option<u64>,
     pub message: Option<String>,
 }
 
 #[serde_with::skip_serializing_none]
-#[derive(Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Builder, Default, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[builder(
+    default,
+    setter(into, strip_option),
+    build_fn(private, name = "fallible_build")
+)]
 pub struct Warning {
     pub code: Option<u64>,
     pub message: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Builder, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[builder(setter(into, strip_option), build_fn(private, name = "fallible_build"))]
 pub struct Info {
     pub message: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Builder, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[builder(setter(into, strip_option), build_fn(private, name = "fallible_build"))]
 pub struct Debug {
     pub message: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Builder, Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[builder(setter(into, strip_option), build_fn(private, name = "fallible_build"))]
 pub struct Verbose {
     pub message: String,
+}
+
+crate::gen_builder_method! {
+    ErrorBuilder   => Error;
+    WarningBuilder => Warning;
+    InfoBuilder    => Info;
+    DebugBuilder   => Debug;
+    VerboseBuilder => Verbose;
 }
