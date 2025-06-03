ea76b7a6e0a3844cb0de0564afb7fb3b20488848
eareimu <reimu@genmeta.net>
2025-06-03 19:34:08 +0800

    fix(qbase): fixed server panic on verify_client_params failed

--- qbase/src/param.rs
+++ qbase/src/param.rs
@@ -509,7 +509,6 @@ impl ArcParameters {
             Ok(remote_params) => Ok(remote_params),
             Err(error) => {
                 params.wake_all();
-                *guard = Err(error.clone());
                 Err(error)
             }
         }
@@ -530,6 +529,10 @@ impl ArcParameters {
     /// When some connection error occurred, convert this parameters
     /// into error state.
     pub fn on_conn_error(&self, error: &Error) {
+        tracing::warn!(
+            ?error,
+            "connection error, convert parameters to error state"
+        );
         let mut guard = self.0.lock().unwrap();
         if let Ok(params) = guard.deref_mut() {
             params.wake_all();
