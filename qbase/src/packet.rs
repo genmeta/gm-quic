fab63c7aa8b3112534f78792236e4ec28517969a
eareimu <reimu@genmeta.net>
2025-07-25 18:36:50 +0800

    feat(qinterface): add poll_close for QuicIO, fix possiable deadlock

--- qbase/src/packet.rs
+++ qbase/src/packet.rs
@@ -169,7 +169,7 @@ impl Iterator for PacketReader {
         match io::be_packet(&mut self.raw_bytes, self.dcid_len) {
             Ok(packet) => Some(Ok(packet)),
             Err(e) => {
-                tracing::debug!(error = ?e, "dropped unparsed packet");
+                tracing::debug!(error = ?e, "Dropped unparsed packet");
                 self.raw_bytes.clear(); // no longer parsing
                 Some(Err(e))
             }
