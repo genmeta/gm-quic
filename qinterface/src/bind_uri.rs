89b6c88e1fef17789f31a11c8b4c382fb8e0ddab
eareimu <reimu@genmeta.net>
2026-03-09 14:55:10 +0800

    refactor(qinterface): remove redundant Arc wrapper from BindUri
    Uri internally uses Bytes (Arc-backed), making the outer Arc unnecessary.
    This eliminates an extra heap allocation and pointer indirection.

--- qinterface/src/bind_uri.rs
+++ qinterface/src/bind_uri.rs
@@ -3,7 +3,6 @@ use std::{
     fmt::Display,
     net::{AddrParseError, IpAddr, SocketAddr},
     str::FromStr,
-    sync::Arc,
 };
 
 use derive_more::{Display, Into};
@@ -15,7 +14,7 @@ use qbase::{
 use thiserror::Error;
 
 #[derive(Debug, Display, Clone, Into, PartialEq, Eq, Hash)]
-pub struct BindUri(Arc<Uri>);
+pub struct BindUri(Uri);
 
 #[derive(Debug, Error)]
 pub enum ParseBindUriError {
@@ -108,7 +107,7 @@ impl TryFrom<Uri> for BindUri {
             }
         }
 
-        Ok(Self(Arc::new(uri)))
+        Ok(Self(uri))
     }
 }
 
@@ -214,13 +213,8 @@ impl BindUri {
         parse_ble_bind_uri(&self.0)
     }
 
-    fn uri_mut(&mut self) -> &mut Uri {
-        Arc::make_mut(&mut self.0)
-    }
-
     pub fn add_prop(&mut self, key: &str, value: &str) {
-        let uri = self.uri_mut();
-        let mut uri_parts = uri.clone().into_parts();
+        let mut uri_parts = self.0.clone().into_parts();
         uri_parts.path_and_query = uri_parts.path_and_query.map(|pq| {
             let query = match pq.query() {
                 Some(exist_query) => format!("{exist_query}&{key}={value}"),
@@ -230,7 +224,7 @@ impl BindUri {
                 .parse()
                 .expect("Path and query should be valid")
         });
-        *uri = Uri::from_parts(uri_parts).expect("BindUri should be valid");
+        self.0 = Uri::from_parts(uri_parts).expect("BindUri should be valid");
     }
 
     pub const ALLOC_PORT_ID: &'static str = "alloc_port_id";
